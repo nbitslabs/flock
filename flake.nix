7a8f4ca9fd806b2743fa46fc244ef28246aad40f
Raghav Sood <r@raghavsood.com>
2026-03-05 11:20:10 +0800

    flake: init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  languages.go = {
+                    enable = true;
+                  };
+
+                  packages = with pkgs; [
+                    sqlc
+                    goose
+                    python3
+                    sqlite
+                    tailwindcss
+                  ];
+
+                  enterShell = ''
+                    echo "flock shell started!"
+                  '';
+                }
+              ];
+            };
+          });
+    };
+}
